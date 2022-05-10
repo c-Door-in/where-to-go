2060ba6a499fc0cd79379ebfc874326019836f59
c_Door in <egor1@mail.ru>
2022-05-10 23:13:25 +0300

    Add Place model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from places.models import Place, Coordinate
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'coordinates',
+    )
+
+
+admin.site.register(Coordinate)
--- places/models.py
+++ places/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-print('hello!')
+
+class Coordinate(models.Model):
+    lng = models.CharField('Долгота', max_length=50)
+    lat = models.CharField('Широта', max_length=50)
+
+    def __str__(self):
+        return f'{self.lng}, {self.lat}'
+
+
+class Place(models.Model):
+    title = models.CharField('Название', max_length=100)
+    coordinates = models.OneToOneField(
+        Coordinate,
+        on_delete=models.SET_NULL,
+        related_name='places',
+        verbose_name='Координаты',
+        null=True,
+    )
+    description_short = models.TextField('Короткое описание', max_length=300)
+    description_long = models.TextField('Полное описание')
+
+    def __str__(self):
+        return self.title
